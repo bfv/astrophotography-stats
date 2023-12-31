7c8a5cf8acae76a9eab5f92a715e52326c68aca3
Bronco Oostermeyer <dev@bfv.io>
2023-12-31 13:45:47 +0100

    added progress bar; removed debug prints

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
 import tomllib
+
 from photodir import *
 from generate_stats import *
+from tqdm import tqdm
 
 ignore_dirs = []
 config = {}
@@ -9,11 +11,9 @@ files = []
 
 def main():
     config = init_config()
-    #print(config)
     photo_dir = get_photo_dir(config)
     subdirs = get_subdirs(photo_dir)
     eval_dirs(photo_dir, subdirs)
-    #print("files found:", len(files))
     print_stats()
     
 def init_config() -> {}:
@@ -33,17 +33,18 @@ def get_photo_dir(config) -> str:
     default_dir = input(f"Input photo dir [{default_dir}]: ") or default_dir   
     return default_dir
 
-def get_subdirs(dir) -> []:
+def get_subdirs(dir: str) -> []:
     evaldirs = os.listdir(dir)
-    subdirs = [i for i in evaldirs if i not in ignore_dirs]
+    subdirs = [dir for dir in evaldirs if dir not in ignore_dirs]
     return subdirs
 
 def eval_dirs(rootdir, subdirs):
-    for subdir in subdirs:
+    for subdir in tqdm(subdirs, desc ="processing directories", write_bytes=False):
+        #subdir = subdirs[i]
         dir = os.path.join(rootdir, subdir)
         eval_dir(dir)
 
-def eval_dir(dir):
+def eval_dir(dir: str):
    pdir = PhotoDir(dir)
    files.extend(pdir.photos)
 
